30eaf0f1ce9a5dbf2a1fac62f01df192c4e25b7a
Thomas Beckenhauer <thomasbeckenhauer@gmail.com>
2014-10-20 18:15:10 -0500

    putting this in a repository

---
+++ Vote-O-Matic.py
@@ -0,0 +1,10 @@
+red, blue, other = range(0, 3, 1)   #assign a positional assignment to candidates
+candidate = [0,0,0]                 #initialize all votes to 0
+vote = ''
+while vote != 4:
+    vote = input ("Vote for your color. Red<0>, Blue<1>, Other<2> or Done<4>") #ask voter for vote
+    candidate[vote==other] += 1     #if vote = other, increment other
+    candidate[vote==blue] += 1      #if vote = blue, increment blue
+    candidate[vote==red] += 1       #if vote = red, increment red
+print {'other': candidate[other],'blue' : candidate[blue],'red'  : candidate[red]} #display results
+vote = input ("") #ask voter for vote
